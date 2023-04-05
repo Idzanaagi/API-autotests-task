fabf917a2710978cf90e93177728cb295402a8a7
idzanaagi <anmorgunowa@gmail.com>
2023-04-06 01:17:55 +0300

    docs: update test description

--- src/test/java/api/LimitedListTestCase.java
+++ src/test/java/api/LimitedListTestCase.java
@@ -1,48 +1,49 @@
 package api;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import pojo.limitItems.ResponseData;
-
 import static io.restassured.RestAssured.given;
 
-public class CheckListTestCase {
-    public Integer limitItemsCount = 5;
+import pojo.limitItems.ResponseData;
+
+public class LimitedListTestCase {
+    private final Integer limitItemsCount = 5;
     RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("https://pokeapi.co/api/v2//pokemon/")
+            .setBaseUri("https://pokeapi.co/api/v2/pokemon/")
             .addQueryParam("limit", limitItemsCount)
-            .setContentType("ContentType.JSON")
+            .setContentType(ContentType.JSON)
             .build();
 
     @Test
     @DisplayName("CLR-1, check the list restriction with the query parameter limit")
     public void checkTheListRestriction() {
 
-        ResponseData limit = given()
+        ResponseData limitedListData = given()
                 .when()
                 .spec(requestSpec)
                 .get()
                 .then()
                 .extract().as(ResponseData.class);
 
-        Assertions.assertEquals((int) limitItemsCount, limit.getResults().size());
+        Assertions.assertEquals((int) limitItemsCount, limitedListData.getResults().size());
     }
 
     @Test
     @DisplayName("CLR-2, check if each pokemon has a name on the restricted list")
-    public void checkPokemonHasName () {
+    public void checkEveryPokemonHasName () {
 
-        ResponseData limit = given()
+        ResponseData limitedListData = given()
                 .when()
                 .spec(requestSpec)
                 .get()
                 .then()
                 .extract().as(ResponseData.class);
 
-        Assertions.assertTrue(limit.getResults().stream().allMatch(x-> x.getName() != null));
+        Assertions.assertTrue(limitedListData.getResults().stream().allMatch(x-> x.getName() != null));
     }
 
 }
--- src/test/java/api/PokemonPropertiesTestCase.java
+++ src/test/java/api/PokemonPropertiesTestCase.java
@@ -6,15 +6,13 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 import java.util.stream.Collectors;
-
 import static io.restassured.RestAssured.given;
 
 import pojo.main.ResponseData;
 
-public class ComparePokemonPropertiesTest {
+public class PokemonPropertiesTestCase {
     RequestSpecification requestSpec = new RequestSpecBuilder()
             .setBaseUri("https://pokeapi.co/api/v2/")
             .setContentType(ContentType.JSON)
@@ -23,21 +21,21 @@ public class ComparePokemonPropertiesTest {
     @DisplayName("CPC-1, compare pokemon weight")
     public void comparePokemonWeight() {
 
-        ResponseData rattataData = given()
+        ResponseData rattataPokemonData = given()
                 .when()
                 .spec(requestSpec)
                 .get("pokemon/rattata")
                 .then()
                 .extract().as(ResponseData.class);
 
-        ResponseData pidgeottoData = given()
+        ResponseData pidgeottoPokemonData = given()
                 .when()
                 .spec(requestSpec)
                 .get("pokemon/pidgeotto")
                 .then()
                 .extract().as(ResponseData.class);
 
-        Assertions.assertTrue(rattataData.getWeight() < pidgeottoData.getWeight());
+        Assertions.assertTrue(rattataPokemonData.getWeight() < pidgeottoPokemonData.getWeight());
     }
 
     @Test
@@ -45,24 +43,24 @@ public class ComparePokemonPropertiesTest {
     public void comparePokemonAbility() {
         String expectedAbility = "run-away";
 
-        ResponseData rattataData = given()
+        ResponseData rattataPokemonData = given()
                 .when()
                 .spec(requestSpec)
                 .get("pokemon/rattata")
                 .then()
                 .extract().as(ResponseData.class);
 
-        ResponseData pidgeottoData = given()
+        ResponseData pidgeottoPokemonData = given()
                 .when()
                 .spec(requestSpec)
                 .get("pokemon/pidgeotto")
                 .then()
                 .extract().as(ResponseData.class);
 
-        List<String> getRattataAbility = rattataData.getAbilities().stream().map(x -> x.getAbility().getName()).collect(Collectors.toList()) // получение способностей
+        List<String> getRattataAbility = rattataPokemonData.getAbilities().stream().map(x -> x.getAbility().getName()).collect(Collectors.toList()) // получение способностей
                 .stream().filter(x-> x.contains(expectedAbility)).collect(Collectors.toList()); // фильтрация
 
-        List<String> getPidgeottoAbility = pidgeottoData.getAbilities().stream().map(x -> x.getAbility().getName()).collect(Collectors.toList())
+        List<String> getPidgeottoAbility = pidgeottoPokemonData.getAbilities().stream().map(x -> x.getAbility().getName()).collect(Collectors.toList())
                 .stream().filter(x-> x.contains(expectedAbility)).collect(Collectors.toList());
 
         Assertions.assertTrue(getRattataAbility.size() > 0); // проверка, что rattata имеет способность run-away
