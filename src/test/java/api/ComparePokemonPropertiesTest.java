b2399a04cc817e1fd58bfef65e1b4f42dabcb5aa
idzanaagi <anmorgunowa@gmail.com>
2023-04-05 20:40:25 +0300

    feat: add compare pokemon property tests (check ability)

--- src/test/java/api/ComparePokemonPropertiesTest.java
+++ src/test/java/api/ComparePokemonPropertiesTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static io.restassured.RestAssured.given;
 
 public class ComparePokemonPropertiesTest {
@@ -12,23 +15,53 @@ public class ComparePokemonPropertiesTest {
     @Test
     @DisplayName("CPC-1, compare pokemon weight")
     public void comparePokemonWeight() {
-
         String URL = "https://pokeapi.co/api/v2/";
-        pojo.main.ResponseData rattata = given()
+
+        pojo.main.ResponseData rattataData = given()
                 .when()
                 .contentType(ContentType.JSON)
                 .get(URL+"pokemon/rattata")
                 .then()
                 .extract().as(pojo.main.ResponseData.class);
 
-        pojo.main.ResponseData pidgeotto = given()
+        pojo.main.ResponseData pidgeottoData = given()
                 .when()
                 .contentType(ContentType.JSON)
                 .get(URL+"pokemon/pidgeotto")
                 .then()
                 .extract().as(pojo.main.ResponseData.class);
 
-        Assertions.assertTrue(rattata.getWeight() < pidgeotto.getWeight());
+        Assertions.assertTrue(rattataData.getWeight() < pidgeottoData.getWeight());
+    }
+
+    @Test
+    @DisplayName("CPC-2, compare pokemon ability: run-away")
+    void comparePokemonAbility() {
+        String URL = "https://pokeapi.co/api/v2/";
+        String expectedAbility = "run-away";
+
+        pojo.main.ResponseData rattataData = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .get(URL+"pokemon/rattata")
+                .then()
+                .extract().as(pojo.main.ResponseData.class);
+
+        pojo.main.ResponseData pidgeottoData = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .get(URL+"pokemon/pidgeotto")
+                .then()
+                .extract().as(pojo.main.ResponseData.class);
+
+        List<String> getRattataAbility = rattataData.getAbilities().stream().map(x -> x.getAbility().getName()).collect(Collectors.toList()) // получение способностей
+                .stream().filter(x-> x.contains(expectedAbility)).collect(Collectors.toList()); // фильтрация
+
+        List<String> getPidgeottoAbility = pidgeottoData.getAbilities().stream().map(x -> x.getAbility().getName()).collect(Collectors.toList())
+                .stream().filter(x-> x.contains(expectedAbility)).collect(Collectors.toList());
+
+        Assertions.assertTrue(getRattataAbility.size() > 0); // проверка, что rattata имеет способность run-away
+        Assertions.assertEquals(0, getPidgeottoAbility.size()); // проверка, что pidgeotto не имеет способность run-away
     }
 
 }
