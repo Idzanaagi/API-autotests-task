bff1266686d86986f55d84c69f02f4a5d5d3a0cc
idzanaagi <anmorgunowa@gmail.com>
2023-04-07 10:45:50 +0300

    style: update formatting

--- src/test/java/helpers/RequestWrapperHelper.java
+++ src/test/java/helpers/RequestWrapperHelper.java
@@ -5,6 +5,7 @@ import io.restassured.response.ValidatableResponse;
 import io.restassured.response.ValidatableResponseOptions;
 import io.restassured.specification.RequestSpecification;
 import static io.restassured.RestAssured.given;
+
 public class RequestWrapperHelper {
     public static ValidatableResponseOptions<ValidatableResponse, Response> setRequest(RequestSpecification spec, String url) {
         return given()
